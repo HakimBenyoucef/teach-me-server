0491a5e13b27d4d5b10ffa47283ae90e32192ae0
Hakim Benyoucef <benyoucef.hakim@gmail.com>
2020-11-09 01:25:20 +0100

    add peerjs

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
+const { PeerServer } = require("peer");
+
+/** Peer server config */
+const peerServer = new PeerServer({ port: 5001, path: "/" });
+peerServer.on("connection", (client) => {
+  console.log("User connected with #", client.id);
+});
+peerServer.on("disconnect", (client) => {
+  console.log("User disconnected with #", client.id);
+});
+/** END Peer server config */
 
 const io = require("socket.io")(server);
 
